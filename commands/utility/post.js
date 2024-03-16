5b318de2c16988ee883460c8939961ada265ca0b
rafael.pereira <rafael.pereira@medicalsys.com.br>
2024-03-16 07:16:33 -0300

    log working

---
+++ commands/utility/post.js
@@ -0,0 +1,95 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("node:fs");
+
+// File to store messages
+//const messageFile = "messages.txt";
+const messageFile = "update.lua";
+
+let log_channel = null;
+
+const alphabetRegex = /^[^a-zA-Z]+$/;
+function treat(value) {
+  value = value.trim();
+  value = value.toLowerCase();
+  value = value.replace(alphabetRegex, "");
+  return value;
+}
+/*
+function modifyAndAppendLine(newItem) {
+  fs.readFile(messageFile, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const lines = data.split('\n');
+    const lastLine = lines[lines.length - 1];
+    const modifiedLine = lastLine.replace('}', `, ${newItem}`); // Replace } with comma and new item
+    lines[lines.length - 1] = modifiedLine + '}'; // Replace last line with modified version and closing }
+
+    const updatedData = lines.join('\n');
+
+    fs.writeFile(messageFile, updatedData, (err) => {
+      if (err) throw err;
+      console.log(`Successfully appended new item to ${messageFile}`);
+    });
+  });
+}
+*/
+// Replace with your file path and desired item
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("post")
+    .setDescription("add mod to spotlight")
+    .addStringOption((option) =>
+      option
+        .setName("modid")
+        .setDescription("mod id or way to identify mod")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("twitter")
+        .setDescription(
+          "msg with 140 length max to summary or show how useful can mod be"
+        )
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    let modid = interaction.options.getString("modid");
+    let twitter = interaction.options.getString("twitter");
+    modid = treat(modid);
+    twitter = treat(twitter);
+    // Defer the initial reply with an ephemeral message
+    await interaction.deferReply({ ephemeral: true });
+
+    // Send an ephemeral follow-up message with processing information
+    const followUpMessage = `processing ${modid}: ${twitter}`;
+    await interaction.followUp({ content: followUpMessage, ephemeral: true });
+
+    if (twitter.length > 140) {
+      await interaction.editReply("twitter too big");
+      return;
+    }
+    if (modid.length > 80) {
+      await interaction.editReply("modid too big");
+      return;
+    }
+
+    if (log_channel == null) {
+      log_channel = await interaction.guild.channels.fetch(
+        "1218305100494409909"
+      );
+    }
+    let new_item = `${modid}=${twitter}`;
+    await log_channel.send(`<@!${interaction.user.id}>: ${new_item}`);
+
+    //modifyAndAppendLine(`${new_item}`);
+
+    fs.appendFile(messageFile, `${new_item}\n`, (err) => {
+      if (err) {
+        console.error(err);
+        return interaction.editReply("Error saving message!");
+      }
+      console.log(`Message appended to ${messageFile}`);
+      interaction.editReply(modid + " twitter saved successfully!");
+    });
+  },
+};
