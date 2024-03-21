14fe5ce918eab284443fd41a9b348c38d6bce257
rafael.pereira <rafael.pereira@medicalsys.com.br>
2024-03-21 00:01:44 -0300

    use scrape and workshop id

--- commands/utility/post.js
+++ commands/utility/post.js
@@ -1,10 +1,14 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fs = require("node:fs");
+const scrape = require("../../lib/webscrapping")
 
 // File to store messages
 //const messageFile = "messages.txt";
 const messageFile = "update.lua";
-const MAX_ID = 20, MAX_TWITTER=50;
+const MAX_ID = 20,
+  MAX_TWITTER = 50;
+
+const hint_recycleMSG = "press UP, to restore your last message";
 
 let log_channel = null;
 
@@ -13,7 +17,7 @@ function treat(value) {
   value = value.trim();
   value = value.toLowerCase();
   value = value.replace(alphabetRegex, ""); //only alphabetic
-  value = value.replace(/(.)\1{2,}/g, '$1$1'); //trim repeated values
+  value = value.replace(/(.)\1{2,}/g, "$1$1"); //trim repeated values
   return value;
 }
 /*
@@ -42,36 +46,50 @@ module.exports = {
     .setDescription("add mod to spotlight")
     .addStringOption((option) =>
       option
-        .setName("modid")
-        .setDescription("mod id or way to identify mod")
+        .setName("workshop_id")
+        .setDescription("workshop_id")
         .setRequired(true)
     )
     .addStringOption((option) =>
       option
-        .setName("short-desc")
+        .setName("short_desc")
         .setDescription(
           `msg with ${MAX_TWITTER} length max to summary or show how useful can mod be`
         )
         .setRequired(true)
     ),
   async execute(interaction) {
-    let modid = interaction.options.getString("modid");
-    let short-desc = interaction.options.getString("short-desc");
-    modid = treat(modid);
-    short-desc = treat(short-desc);
-    // Defer the initial reply with an ephemeral message
+    let modid = interaction.options.getString("workshop_id");
+    let short_desc = interaction.options.getString("short_desc");
     await interaction.deferReply({ ephemeral: true });
-
-    // Send an ephemeral follow-up message with processing information
-    const followUpMessage = `processing ${modid}: ${short-desc}`;
+    const followUpMessage = `processing ${modid}: ${short_desc}`;
     await interaction.followUp({ content: followUpMessage, ephemeral: true });
 
-    if (short-desc.length > MAX_TWITTER) {
-      await interaction.editReply("short-desc too big");
+    if(!/^\d+$/.test(modid)) await interaction.editReply("add workshop id, only numbers");
+    let mod_data = await scrape.getModData(modid);
+    if(mod_data.subscriber_count > 109999){
+      await interaction.editReply(`this mod is too popular >100k subs, ${hint_recycleMSG}`);
+      return;
+
+    }
+    if(mod_data.subscriber_count<300){
+      await interaction.editReply(`this mod is yet too small <300 subs, ${hint_recycleMSG}`);
+      return;
+    }
+    modid = mod_data.modName;
+    //modid = treat(modid);
+    short_desc = treat(short_desc);
+    // Defer the initial reply with an ephemeral message
+
+    // Send an ephemeral follow-up message with processing information
+
+
+    if (short_desc.length > MAX_TWITTER) {
+      await interaction.editReply(`short_desc too big, ${hint_recycleMSG}`);
       return;
     }
     if (modid.length > MAX_ID) {
-      await interaction.editReply("modid too big");
+      await interaction.editReply(`modid too big, ${hint_recycleMSG}`);
       return;
     }
 
@@ -80,7 +98,7 @@ module.exports = {
         "1218305100494409909"
       );
     }
-    let new_item = `${modid}=${short-desc}`;
+    let new_item = `${modid}=${short_desc}`;
     await log_channel.send(`<@!${interaction.user.id}>: ${new_item}`);
 
     //modifyAndAppendLine(`${new_item}`);
@@ -88,10 +106,10 @@ module.exports = {
     fs.appendFile(messageFile, `${new_item}\n`, (err) => {
       if (err) {
         console.error(err);
-        return interaction.editReply("Error saving message!");
+        return interaction.editReply(`Error saving message! ${hint_recycleMSG}`);
       }
       console.log(`Message appended to ${messageFile}`);
-      interaction.editReply(modid + " short-desc saved successfully!");
+      interaction.editReply(modid + " short_desc saved successfully!");
     });
   },
 };
